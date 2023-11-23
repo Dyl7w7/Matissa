479b93170dd8a492a209bcd21017ef40db393b1d
Dylan <dilanstivel9@gmail.com>
2023-11-23 18:48:02 -0500

    Agregar archivos de proyecto.

---
+++ Matissa/Models/DetallePedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matissa.Models
+{
+    public partial class DetallePedido
+    {
+        public int IdDetallePedido { get; set; }
+        public int IdProducto { get; set; }
+        public int IdPedido { get; set; }
+        public int CantidadProducto { get; set; }
+        public double PrecioUnitario { get; set; }
+
+        public virtual Pedido IdPedidoNavigation { get; set; } = null!;
+        public virtual Producto IdProductoNavigation { get; set; } = null!;
+    }
+}
